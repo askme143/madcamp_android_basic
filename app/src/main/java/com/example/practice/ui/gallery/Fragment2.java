7f8abfc1ca76ba13839f8cda25fb74b16f2ac567
YookyungOh <gamtangerine@gmail.com>
2020-07-09 15:01:19 +0900

    GridView

--- app/src/main/java/com/example/practice/ui/gallery/Fragment2.java
+++ app/src/main/java/com/example/practice/ui/gallery/Fragment2.java
@@ -4,10 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+import android.content.Intent;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView;
 
 import com.example.practice.R;
 
@@ -18,7 +22,27 @@ public class Fragment2 extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                               @Nullable Bundle savedInstanceState) {
-        viewGroup = (ViewGroup) inflater.inflate(R.layout.fragment2, container, false);
-        return viewGroup;
+        View view = (View) inflater.inflate(R.layout.fragment2, container, false);
+
+        GridView gridView = (GridView) view.findViewById(R.id.grid_view);
+
+        // Instance of ImageAdapter Class
+        gridView.setAdapter(new ImageAdapter(getActivity()));
+
+        //// On Click event for Single GridView Item ////
+        gridView.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+
+                // Sending image id to FullScreenActivity
+                Intent i = new Intent(getActivity(), FullImageActivity.class);
+                // passing array index
+                i.putExtra("id", position);
+                startActivity(i);
+            }
+        });
+
+        return view;
     }
 }
---
+++ app/src/main/java/com/example/practice/ui/gallery/FullImageActivity.java
@@ -0,0 +1,28 @@
+package com.example.practice.ui.gallery;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.example.practice.R;
+
+public class FullImageActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment2_full_image);
+
+        // get intent data
+        Intent i = getIntent();
+
+        // Selected image id
+        int position = i.getExtras().getInt("id");
+        ImageAdapter imageAdapter = new ImageAdapter(this);
+
+        ImageView imageView = (ImageView) findViewById(R.id.full_image_view);
+        imageView.setImageResource(imageAdapter.mThumbIds[position]);
+    }
+
+}
