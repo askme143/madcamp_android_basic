2c32b98ec3feee05ad647e0e32704dd2652ac4c6
Yeongil Yoon <askme143@kaist.ac.kr>
2020-07-09 22:43:58 +0900

    Fix compare problem in the class Contact

--- app/src/main/java/com/example/practice/PermissionRequestActivity.java
+++
@@ -1,7 +0,0 @@
-package com.example.practice;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class PermissionRequestActivity extends AppCompatActivity {
-
-}
--- app/src/main/java/com/example/practice/ui/contacts/Contact.java
+++ app/src/main/java/com/example/practice/ui/contacts/Contact.java
@@ -53,4 +53,22 @@ public class Contact {
     public int getID() {
         return id;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Contact contact = (Contact) o;
+
+        if (!name.equals(contact.name)) return false;
+        return phoneNumber.equals(contact.phoneNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + phoneNumber.hashCode();
+        return result;
+    }
 }
