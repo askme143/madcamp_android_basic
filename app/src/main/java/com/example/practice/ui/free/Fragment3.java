580f5841a46b5c80d168d49d9892acfbae6693b9
YookyungOh <gamtangerine@gmail.com>
2020-07-10 14:18:21 +0900

    samsung health

--- app/src/main/java/com/example/practice/ui/free/Fragment3.java
+++ app/src/main/java/com/example/practice/ui/free/Fragment3.java
@@ -1,18 +1,44 @@
 package com.example.practice.ui.free;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.TextView;
+
+import com.samsung.android.sdk.healthdata.HealthConnectionErrorResult;
+import com.samsung.android.sdk.healthdata.HealthConstants;
+import com.samsung.android.sdk.healthdata.HealthData;
+import com.samsung.android.sdk.healthdata.HealthDataResolver;
+import com.samsung.android.sdk.healthdata.HealthDataStore;
+import com.samsung.android.sdk.healthdata.HealthPermissionManager;
+import com.samsung.android.sdk.healthdata.HealthPermissionManager.PermissionKey;
+import com.samsung.android.sdk.healthdata.HealthResultHolder;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 import com.example.practice.R;
 
 public class Fragment3 extends Fragment {
-    ViewGroup viewGroup;
+    /*ViewGroup viewGroup;
 
     @Nullable
     @Override
@@ -21,6 +47,218 @@ public class Fragment3 extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         viewGroup = (ViewGroup) inflater.inflate(R.layout.fragment3, container, false);
         return viewGroup;
+    }*/
+
+    public static final String APP_TAG = "Practice_SimpleHealth";
+    private HealthDataStore mStore;
+    private HealthConnectionErrorResult mConnError;
+    private Set<PermissionKey> mKeySet;
+
+    TextView textView;
+
+    public Fragment3() {} ///함수 정의하는건가??
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = (View) inflater.inflate(R.layout.fragment3, container, false);
+
+        TextView textView = (TextView) getActivity().findViewById(R.id.StepCount);
+
+        mKeySet = new HashSet<>();
+        mKeySet.add(new PermissionKey(HealthConstants.StepCount.HEALTH_DATA_TYPE, HealthPermissionManager.PermissionType.READ));
+        // Create a HealthDataStore instance and set its listener
+        mStore = new HealthDataStore(getActivity(), mConnectionListener);
+        // Request the connection to the health data store
+        mStore.connectService();
+
+        return textView;
+
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    private final HealthDataStore.ConnectionListener mConnectionListener = new HealthDataStore.ConnectionListener() {
+
+        @RequiresApi(api = Build.VERSION_CODES.O)
+        @Override
+        public void onConnected() {
+            Log.d(APP_TAG, "Health data service is connected.");
+            HealthPermissionManager pmsManager = new HealthPermissionManager(mStore);
+
+            try {
+                // Check whether the permissions that this application needs are acquired
+                // Request the permission for reading step counts if it is not acquired
+                Map<PermissionKey, Boolean> resultMap = pmsManager.isPermissionAcquired(mKeySet);
+                // Get the current step count and display it if data permission is required
+                // ...
+
+                if (resultMap.containsValue(Boolean.FALSE)) {
+                    // Request the permission for reading step counts if it is not acquired
+                    pmsManager.requestPermissions(mKeySet, getActivity()).setResultListener(mPermissionListener);
+                } else {
+                    // Get the current step count and display it
+                    // ...
+
+                    showStepCount();
+                }
+            } catch (Exception e) {
+                Log.e(APP_TAG, e.getClass().getName() + " - " + e.getMessage());
+                Log.e(APP_TAG, "Permission setting fails.");
+            }
+        }
+
+        @Override
+        public void onConnectionFailed(HealthConnectionErrorResult error) {
+            Log.d(APP_TAG, "Health data service is not available.");
+            showConnectionFailureDialog(error);
+        }
+
+        @Override
+        public void onDisconnected() {
+
+            Log.d(APP_TAG, "Health data service is disconnected.");
+        }
+    };
+
+    private void showConnectionFailureDialog(HealthConnectionErrorResult error) {
+
+        AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
+        mConnError = error;
+        String message = "Connection with Samsung Health is not available";
+
+        if (mConnError.hasResolution()) {
+            switch(error.getErrorCode()) {
+                case HealthConnectionErrorResult.PLATFORM_NOT_INSTALLED:
+                    message = "Please install Samsung Health";
+                    break;
+                case HealthConnectionErrorResult.OLD_VERSION_PLATFORM:
+                    message = "Please upgrade Samsung Health";
+                    break;
+                case HealthConnectionErrorResult.PLATFORM_DISABLED:
+                    message = "Please enable Samsung Health";
+                    break;
+                case HealthConnectionErrorResult.USER_AGREEMENT_NEEDED:
+                    message = "Please agree with Samsung Health policy";
+                    break;
+                default:
+                    message = "Please make Samsung Health available";
+                    break;
+            }
+        }
+
+        alert.setMessage(message);
+
+        alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                if (mConnError.hasResolution()) {
+                    mConnError.resolve(getActivity()); //mInstance???
+                }
+            }
+        });
+
+        if (error.hasResolution()) {
+            alert.setNegativeButton("Cancel", null);
+        }
+
+        alert.show();
+    }
+
+    @Override
+    public void onDestroy() {
+        mStore.disconnectService();
+        super.onDestroy();
+    }
+
+    private final HealthResultHolder.ResultListener<HealthPermissionManager.PermissionResult> mPermissionListener =
+            new HealthResultHolder.ResultListener<HealthPermissionManager.PermissionResult>() {
+
+                @RequiresApi(api = Build.VERSION_CODES.O)
+                @Override
+                public void onResult(HealthPermissionManager.PermissionResult result) {
+                    Log.d(APP_TAG, "Permission callback is received.");
+                    Map<PermissionKey, Boolean> resultMap = result.getResultMap();
+
+                    if (resultMap.containsValue(Boolean.FALSE)) {
+                        Log.i(APP_TAG, "SE FUDEU");
+                    } else {
+                        // Get the current step count and display it
+                        showStepCount();
+                    }
+                }
+            };
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void showStepCount() {
+        HealthDataResolver resolver = new HealthDataResolver(mStore, null);
+
+        long startTime = getStartTimeOfToday();
+        long endTime = startTime + ONE_DAY_IN_MILLIS;
+
+        HealthDataResolver.ReadRequest request = new HealthDataResolver.ReadRequest.Builder()
+                .setDataType(HealthConstants.StepCount.HEALTH_DATA_TYPE)
+                .setLocalTimeRange(HealthConstants.StepCount.START_TIME, HealthConstants.StepCount.TIME_OFFSET,
+                        startTime, endTime)
+                .build();
+        try {
+            resolver.read(request).setResultListener(mRdResult);
+        } catch (Exception e) {
+            Log.i(APP_TAG, "Reading health data fails.");
+            e.printStackTrace();
+        }
+    }
+
+    private final HealthResultHolder.ResultListener<HealthDataResolver.ReadResult> mRdResult =
+            new HealthResultHolder.ResultListener<HealthDataResolver.ReadResult>() {
+
+                @RequiresApi(api = Build.VERSION_CODES.O)
+                @Override
+                public void onResult(HealthDataResolver.ReadResult result) {
+
+                    int count = 0;
+
+                    try {
+                        Iterator<HealthData> iterator = result.iterator();
+
+                        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+                        while (iterator.hasNext()) {
+                            HealthData data = iterator.next();
+
+                            String start = dtf.format(Instant.ofEpochMilli(data.getLong(HealthConstants.StepCount.START_TIME)).atZone(ZoneOffset.UTC));
+                            String end = dtf.format(Instant.ofEpochMilli(data.getLong(HealthConstants.StepCount.END_TIME)).atZone(ZoneOffset.UTC));
+
+                            Log.i(APP_TAG, "======= { steps: " + data.getInt(HealthConstants.StepCount.COUNT)
+                                    + ", start: " + start
+                                    + ", end: " + end + " } ");
+                            count += data.getInt(HealthConstants.StepCount.COUNT);
+                        }
+                    } finally{
+                        result.close();
+                        Log.i(APP_TAG, "======= Total step count: " + count);
+                        textView.setText(count+"");
+                    }
+                }
+            };
+
+    private static final long ONE_DAY_IN_MILLIS = 24 * 60 * 60 * 1000L;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private long getStartTimeOfToday() {
+
+        return LocalDate.now().atStartOfDay().toEpochSecond(ZoneOffset.UTC) * 1000;
+
+    }
+
+
+
+
+
+
 
 }
