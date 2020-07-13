ccc46842ffe2ac8315a6eb1a875bb69034bfe872
YookyungOh <gamtangerine@gmail.com>
2020-07-13 16:18:52 +0900

    camera debug

--- app/src/main/java/com/example/practice/ui/gallery/Fragment2.java
+++ app/src/main/java/com/example/practice/ui/gallery/Fragment2.java
@@ -1,6 +1,11 @@
 package com.example.practice.ui.gallery;
 
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,12 +14,18 @@ import android.content.Intent;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView;
 
 import com.example.practice.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class Fragment2 extends Fragment {
     ViewGroup viewGroup;
@@ -47,14 +58,50 @@ public class Fragment2 extends Fragment {
         });
 
         FloatingActionButton fab = view.findViewById(R.id.cameraIcon);
+        Uri uri;
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // OCR
-
+//                Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//                File photoFile = createFile();
+//                Uri providerFileUri = FileProvider.getUriForFile(getActivity(), getPackageName(), photoFile);
+//                i.putExtra(MediaStore.EXTRA_OUTPUT, providerFileUri);
+//                startActivityForResult(i, 1);
+                Intent i = new Intent(getActivity(), Camera.class);
+                i.putExtra("id", 0);
+                startActivity(i);
             }
         });
 
+
         return view;
     }
+
+//    private File createFile() {
+//        String fileName = "profileSample";
+//        File imageDir = getActivity().getDir("profileImages", 0); // 0 = MODE_PRIVATE
+//        return new File (imageDir, fileName+".png");
+//    }
+//
+//    private void saveImageFile(Bitmap profileImage) {
+//        // 중복 제거
+//        boolean deleted = profileImagePath.delete();
+//        Log.w(TAG, "Profile Delete Check : "+ deleted);
+//        FileOutputStream fosImage = null;
+//
+//        try {
+//            fosImage = new FileOutputStream(imagePath);
+//            profileImage.compress(Bitmap.CompressFormat.PNG,100,fosImage);
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } finally {
+//            try {
+//                assert fosImage != null;
+//                fosImage.close();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+
 }
